2ed52d5606eec57c100a60425bcb0dd4e15a0cf6
heipidage <heipidage007@gmail.com>
2017-02-26 21:47:58 +0800

    通知暂停取消通知

--- app/src/main/java/com/swjtu/gcmformojo/QqPausedNotificationReceiver.java
+++ app/src/main/java/com/swjtu/gcmformojo/QqPausedNotificationReceiver.java
@@ -1,8 +1,10 @@
 package com.swjtu.gcmformojo;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 //跳转activity
 public class QqPausedNotificationReceiver extends BroadcastReceiver
@@ -10,6 +12,13 @@ public class QqPausedNotificationReceiver extends BroadcastReceiver
     @Override
     public void onReceive(Context context, Intent intent)
     {
+        Bundle msgNotifyBundle = intent.getExtras();
+        int notifyId = msgNotifyBundle.getInt("notifyId");
+        if (notifyId != -1) {
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.cancel(notifyId);
+        }
+
         context.startActivity(new Intent(context, QqPausedNotificationActivity.class));
     }
 }
